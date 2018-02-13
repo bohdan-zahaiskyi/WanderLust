d58c1a5ed57b90b6777f7e36b8bbbdc0851e7adf
bohdan.zahaiskyi <bohdan.zahaiskyi@edvantis.com>
2018-02-13 18:51:05 +0200

    changed structure a bit, styled header

--- Wanderlust/src/app/app.module.ts
+++ Wanderlust/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { HttpModule} from '@angular/http';
 
 import { WandersService } from './wanders.service';
-import { AppComponent } from './main-page/app';
+import { AppComponent } from './app';
 import { WanderSearchComponent } from './main-page/wander-search/wander-search';
 import { WanderResultsComponent } from './main-page/wander-results/wander-results';
+import { MainPageComponent } from './main-page/main-page';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainPageComponent,
     WanderSearchComponent,
     WanderResultsComponent
   ],
